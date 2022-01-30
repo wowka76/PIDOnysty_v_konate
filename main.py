5ba4c7e5af413a4d8e2c721e045416be219db784
aydolov <vladimirik96yar@gmail.com>
2022-01-30 18:01:12 +0300

    really shitty code

---
+++ main.py
@@ -0,0 +1,11 @@
+a = int(input())
+
+if a % 10 == 0 or 5 == a <= 15 or a % 10 == 5 or a % 10 == 6 or a % 10 == 7 or a % 10 == 8 or a % 10 == 9 or 11 <= a <= 14 or 111 <= a <= 114 or 211 <= a <= 214 or 311 <= a <= 314 or 411 <= a <= 414 or 511 <= a <= 514 or 611 <= a <= 614 or 711 <= a <= 714 or 811 <= a <= 814 or 911 <= a <= 914:
+    a = str(a)
+    print(a, "программистов")
+elif a == 1 or a % 10 == 1:
+    a = str(a)
+    print(a, "программист")
+elif 2 <= a <= 4 or a % 10 == 2 or a % 10 == 3 or a % 10 == 4:
+    a = str(a)
+    print(a, "программиста")
